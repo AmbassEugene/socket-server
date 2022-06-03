6f83656d6518f707956708161cf9033906c5d55a
Ambass Eugene <eugene.ambass@hotmail.com>
2022-06-04 00:07:32 +0530

    feat: adds sockets, emits andreceive events from client :hammer:

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require("express");
-const cors = require("cors");
-const http = require("http");
+const { createServer } = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
-
-const server = http.createServer(app);
-
+const httpServer = createServer(app);
 const PORT = process.env.PORT || 5000;
 
-const io = new Server(app, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["POST", "GET"],
-  },
+const io = new Server(httpServer, {
+  /* options */
+  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
 });
 
-server.listen(PORT, () => console.log("Server started at PORT: ", PORT));
+io.on("connection", (socket) => {
+  console.log("socket connection valid: ", {
+    connected: socket.connected,
+    id: socket.id,
+  });
+
+  socket.on("send_message", (data) => {
+    console.log("message event received: ", data);
+    socket.emit("send_message", data);
+  });
+});
+
+httpServer.listen(PORT, () => console.log("listening on port:", PORT));
